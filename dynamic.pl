23f01f7493a57cf33ee3234cde9f52ced3ac3c74
ixchel <ixchelzg@gmail.com>
2014-09-20 01:23:33 -0500

    metodo de eliminar

---
+++ dynamic.pl
@@ -0,0 +1,22 @@
+:- dynamic capital_of/2.
+
+capital_of(listing, stop).
+capital_of(hawaii, honolulu).
+capital_of(ecuador, quito).
+capital_of(hawaii, honolulu).
+capital_of(suiza, berna).
+capital_of(chile, santiago).
+capital_of(eeuu, washington).
+capital_of(estados_unidos, washington).
+capital_of(italia, roma).
+capital_of(francia, paris).
+capital_of(austria, viena).
+capital_of(alemania, berlin).
+capital_of(espania, madrid).
+capital_of(peru, lima).
+capital_of(mexico, ciudad_de_mexico).
+capital_of(ecuador, quito).
+capital_of(argentina, buenosAigres).
+capital_of(colombia, bogota).
+capital_of(listing, stop).
+capital_of([a,s,d,e,r]).
--- probandoprolog.pl
+++ probandoprolog.pl
@@ -1,10 +1,43 @@
+:- initialization(main).
 :- op(15, xfx, '=>').
 =>(X,Y).
+
+main:-
+	consult('/home/ixchel/git/UNAM/IA/Proyecto/Manejo_de_archivos/main.pl').
+rb1(Y):-
+	open_kb('/home/ixchel/git/UNAM/IA/Proyecto/Manejo_de_archivos/bd.txt',Y).
+guardarBD(Y):-
+	save_kb('/home/ixchel/git/UNAM/IA/Proyecto/Manejo_de_archivos/bd.txt',Y).
+
+
+my_remove_one_element(X, [X|Xs], Xs).
+
+my_remove_one_element(X, [Y|Ys], [Y|Zs]):-
+          my_remove_one_element(X, Ys, Zs).
+
 primertermino(X,Y):- X = W=>Z, Y=W.
 segundotermino(X,Y):- X = W=>Z, Y=Z.
 valor(X,[],Y):- fail.
-valor(X,[H|T],Y):- primertermino(H,Z), X=Z, segundotermino(H,W), Y=W, !.
+valor(X,[H|T],Y):- primertermino(H,X), segundotermino(H,Y), !.
 valor(X,[H|T],Y):- primertermino(H,Z), X\=Z, valor(X,T,Y).
+
+
+props(X,Y):- 
+	slice(3,3,X,Y).
+rels(X,Y):-
+	slice(4,4,X,Y).
+
+
+valor(X,[H|T],Y):- primertermino(H,X), segundotermino(H,Y), !.
+valor(X,[H|T],Y):- primertermino(H,Z), X\=Z, valor(X,T,Y).
+
+slice([X|_],1,1,[X]).
+slice([X|T],1,K,[X|T1]) :- K > 1, 
+   K1 is K - 1, slice(T,1,K1,T1).
+slice([_|T],I,K,Y) :- I > 1, 
+   I1 is I - 1, K1 is K - 1, slice(T,I1,K1,Y).
+
+
 cabeza([H|T],Y):- Y=H.
 cola([H|T],Y):- Y=T.
 cabeza(X,Y):- Y=X.
@@ -102,24 +135,167 @@ sacaObjetosHijosDeClase(X,[H|T],Y):-
 								sacaIndividuosDeClase(H,Y),!;
 								sacaObjetosHijosDeClase(X,T,Y),!.
 
-								
-								
-								
-sacaObjetosHijosDeClaseRecursivoInicioFlat(X,Y):- sacaObjetosHijosDeClaseRecursivoInicio(X,Z), flatten(Z,Y).
-
 sacaObjetosHijosDeClaseRecursivoInicio(X,Y):-
 								sacaClasesHijasRecursivoInicioFlat(X,S), 
-								sacaObjetosHijosDeClaseRecursivo(X,S,Y),!.
+								write('S: '),
+								write(S),
+								nl,
+								cabeza(S,U),
+								write('U: '),
+								write(U),
+								nl,
+								sacaObjetosHijosDeClaseRecursivo(X,S,Y).
+
+
+
 
 sacaObjetosHijosDeClaseRecursivo(X,[],Y):- Y=[].						
-sacaObjetosHijosDeClaseRecursivo(X,[H|T],Y):- 	
-								sacaObjetosHijosDeClaseInicio(H,F),
-								sacaObjetosHijosDeClaseRecursivo(X,T,G),
-								anade(F,G,Y),!.
+sacaObjetosHijosDeClaseRecursivo(X,[],Y):- 	
+								
+								write('S: '),
+								write(S),
+								nl
+								.
 
 
+sus(_,_,[],[]).
+sus(X,Y,[X|T],[Y|S]):-!,sus(X,Y,T,S).
+sus(X,Y,[Z|T],[Z|S]):-sus(X,Y,T,S).
+
+eliminaClase(X,[X|T],T).
+eliminaClase(X,[H|T],[H|T1]):-
+	eliminaClase(X,T,T1).
+
+concatenar([],L,L).
+concatenar([X|L1],L2,[X|L3]):-concatenar(L1,L2,L3).
 
 
+elimina(X):-
+	rb1(W),
+	MM=[nombre=>X,_,_,_,_],
+	quieroPadre(MM,W,P),
+	nth0(2,P,Pr),
+	nth0(3,P,Rel),
+	valor(padre,P,Pop),
+	(	
+		Pop\=top,
+		crearNuevaListaInicio(X,Pr,Rel,Pop),
+		rb1(W1),
+		eliminaClase(MM,W1,S),
+		guardarBD(S)
+	);
+	(
+		crearNuevaListaInicioInd(X)
+	)
+	.
+
+quieroPadre(X,[X|T],X).
+quieroPadre(X,[H|T],Y):-
+	quieroPadre(X,T,Y).
+
+crearNuevaListaInicioInd(X):- rb1(W), crearNuevaListaInd(X,W).
+
+
+crearNuevaListaIndRel(X,W,[]).
+crearNuevaListaIndRel(X,W,TI,[H|T]):-
+	(
+		nth0(2,H,Rels),
+		RR= XXX=>X,
+		nth0(1,H,P2),
+		nth0(0,H,Nm),
+		member(RR,Rels),
+		eliminaClase(RR,Rels,LR),
+		H1=[Nm,P2,LR],
+		rb1(W1),
+		sus(H,H1,TI,S),
+		member(TI,W),
+		sus(T1,S,W,S1),
+		eliminaClase(TI,W,LL),
+		concatenar(LL,S,L2),
+		sus(W,L2,W1,STs),
+		guardarBD(STs),
+		crearNuevaListaIndRel(X,W,TI,T)
+	)
+	;
+	crearNuevaListaIndRel(X,W,TI,T).
+
+
+crearNuevaListaInd(X,[]).
+crearNuevaListaInd(X,[H|T]):-
+	(
+		valor(padre,H,P),
+		P\=top,
+		nth0(4,H,Is),
+		IM = [individuo=>X,_,_],
+		member(IM,Is),
+		eliminaClase(IM,Is,ISs),
+		nth0(2,H,P2),
+		nth0(3,H,R2),
+		valor(nombre,H,Nm),
+		H1=[nombre=>Nm,padre=>P,P2,R2,ISs],
+		rb1(W),
+		sus(H,H1,W,S),
+		guardarBD(S),
+		crearNuevaListaInd(X,T)
+	);
+	(
+	valor(padre,H,P),
+	nth0(3,H,Rel),
+	RR= XXX=>X,
+		(
+			member(RR,Rel),
+			P\=top,
+			eliminaClase(RR,Rel,Rs),
+			nth0(2,H,P2),
+			nth0(4,H,ISs),
+			valor(nombre,H,Nm),
+			H1=[nombre=>Nm,padre=>P,P2,Rs,ISs],
+			rb1(W),
+			sus(H,H1,W,S),
+			guardarBD(S),
+			crearNuevaListaInd(X,T)
+		);
+		(
+			P\=top,
+			nth0(4,H,Inds),
+			crearNuevaListaIndRel(X,H,Inds,Inds),
+			crearNuevaListaInd(X,T)
+		)
+	)
+	;
+	P\=top,crearNuevaListaInd(X,T).
+
+crearNuevaListaInicio(X,M,R,Pop):- rb1(W), crearNuevaLista(X,M,R,Pop,W).
+
+crearNuevaLista(X,M,R,Pop,[]).
+crearNuevaLista(X,M,R,Pop,[H|T]):-
+	(
+	valor(padre,H,X),
+	nth0(2,H,P2),
+	nth0(3,H,R2),
+	concatenar(M,P2,L1),
+	concatenar(R,R2,L2),
+	valor(nombre,H,Nm),
+	H1=[nombre=>Nm,padre=>Pop,L1,L2,[]],
+	rb1(W),
+	sus(H,H1,W,S),
+	guardarBD(S),
+	crearNuevaLista(X,M,R,Pop,T)
+	);(
+	valor(padre,H,P),
+	nth0(3,H,Rel),
+	RR= XXX=>X,
+	member(RR,Rel),
+	eliminaClase(RR,Rel,Rs),
+	nth0(2,H,P2),
+	nth0(4,H,ISs),
+	valor(nombre,H,Nm),
+	H1=[nombre=>Nm,padre=>P,P2,Rs,ISs],
+	rb1(W),
+	sus(H,H1,W,S),
+	guardarBD(S),
+	crearNuevaLista(X,M,R,Pop,T)
+	);crearNuevaLista(X,M,R,Pop,T).
 								
 rb(Y):- Y = [
 	[nombre=>animal, padre=>top, 
